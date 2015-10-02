66b5998809e7db5ebe07eda2fef6e0dd04ac6f62
Vicente de Miguel <spidvmp@gmail.com>
2015-10-02 21:57:08 +0200

    listado de tags

--- routes/apiV1/tags.js
+++ routes/apiV1/tags.js
@@ -2,6 +2,14 @@
 var express = require('express');
 var router = express.Router();
 
+//lista de tags en un array
+var tags=['Work','Lifestyle','Mobile','Motor'];
+
+router.get('/',function(req,res){
+
+    var a=tags.toString();
+    return res.json({ok :true, data:a});
+});
 
 
 
